a7101c55051c9d8839922375b60de80f5e61b9bb
Ben Williams <bwilliams@transurban.com>
2017-06-14 09:20:25 +1000

    cleaning  up meta stuff

--- index.js
+++ index.js
@@ -1,34 +1,65 @@
+"use strict";
 const request = require('request');
 const meta = require('html-metadata');
 const urlList = require('./urls.json');
 
+const urlPrefixLength = 69;
+const delimiter = ';';
+
+let isMeta = false;
+
 // allow pages without certs (for UAT environments)
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-let isMeta = false;
+
+//  Arguments
 process.argv.forEach(function (val, index, array) {
-  if(val === "meta"){
-    isMeta = true;
-  }
+    if(val === "meta"){
+        isMeta = true;
+    }
 });
 
+function getStatus(item){
+    request(item.url, (err, response, body) => {
+        if(response){
+            console.log(item.name + delimiter + " " + getUrl(item.url) + delimiter + " " + response.statusCode)
+        }
+        else{
+            log.push("error: " + item.name + ": " + err);
+        }
+    });
+}
+
+function getUrl(url){
+    return url.slice(urlPrefixLength);
+}
+
 urlList.forEach((item) => {
-        request(item.url, (err, response, body) => {
-            if(response){
-              if(isMeta){
-                meta(item.url, function (err, data) {
-                      const output = `${item.name}, ${data.general.title}, ${data.general.description}`;
-                      console.log(output);
-                  });
-              }
-              else{
-                console.log(item.name + "," + response.statusCode);
-              }
+    if( isMeta) {
+        meta(item.url,(err, data) => {
+            if(data){
+                const generalData = {title: "ERROR", description: "ERROR"};
+                const url = getUrl(item.url);
+                if(data.hasOwnProperty('general')){
+                    generalData.title =  data.general.title;
+                    generalData.description = data.general.description;
+                }
+                if(generalData.title === undefined){
+                    generalData.title = "[NO TITLE]"
+                }
+                if(generalData.description === undefined){
+                 generalData.description = "[NO DESCRIPTION]"
+                }
+                console.log(`${item.name}${delimiter} ${url}${delimiter} ${generalData.title}${delimiter} ${generalData.description}`);
             }
-            else{
-                log.push("error: " + item.name + ": " + err);
+            else {
+                getStatus(item);
             }
         });
+    }
+    else{
+        getStatus(item)
+    }
 });
 
 
--- run.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-echo "Starting test URL"
-
-nohup node index.js > output.log &
-
-echo "fin"
